dffbfd956f3bc8a2d381ca1d2f47c476ee348c71
Alexandre Dubeau <adubeau@c2r5p3.42quebec.com>
2023-01-20 17:39:11 -0500

    added topic reply + added mode guard to topic

--- srcs/commands/Topic.cpp
+++ srcs/commands/Topic.cpp
@@ -14,10 +14,10 @@ bool	Topic::validate(const Message& msg) {
 		return false;
 	}
 	_target = msg.getMiddle().at(0);
-	/*if (!_server->getChannelPtr(_target)->checkMemberModes(msg._client, 'o')) {
+	if (_server->getChannelPtr(_target)->checkModes(TOPIC_SET_OP) && !_server->getChannelPtr(_target)->checkMemberModes(msg._client, C_OP)) {
 		msg._client->reply(ERR_CHANOPRIVSNEEDED(_target));
 		return false;
-	}*/
+	}
 	if (!_server->getChannelPtr(_target)->isMember(msg._client)) {
 		msg._client->reply(ERR_NOTONCHANNEL(_target));
 		return false;
@@ -30,10 +30,10 @@ void	Topic::execute(const Message& msg) {
 		if (msg.getTrailing().empty())
 			msg._client->reply(RPL_TOPIC(_target, _server->getChannelPtr(_target)->getTopic()));
 		if (msg.getTrailing().size() == 1 && msg.getTrailing().at(0) == ':')
-			_server->getChannelPtr(_target)->setTopic(" ");
+			_server->getChannelPtr(_target)->setTopic("");
 		else
 			_server->getChannelPtr(_target)->setTopic(msg.getTrailing());
-
+		msg._client->reply(RPL_TOPIC(_target, _server->getChannelPtr(_target)->getTopic()));
 
 	}
 }
