ae802396ac8085080610379c4cf2acba1fdf616d
Alexandre Dubeau <adubeau@c2r2p3.42quebec.com>
2023-01-25 17:16:45 -0500

    added problematic cmd examples to Names

--- srcs/commands/Names.cpp
+++ srcs/commands/Names.cpp
@@ -8,34 +8,38 @@ Names::Names(Server* server) : Command("names", server) {
 Names::~Names() {
 
 }
-
+/*
+ * issues: /names #new,#new1 duplicates first channel
+ * 			/names #new,#new2, segfaults
+ * 			/names #new, #new1 segfaults
+ * */
 bool	Names::validate(const Message& msg) {
-	if (msg.getMiddle().size() < 2) {
+	if (!msg.hasMiddle() || msg.getMiddle().at(0).size() < 1) {
 		_hasTarget = false;
 	}
 	else {
 		_hasTarget = true;
 		_target = msg.getMiddle().at(0);
-		std::string::difference_type n = std::count(_target.begin(), _target.end(), ',');
-
-		if (n > 0)
+		size_t pos;
+		while ((pos = _target.find(',')) != std::string::npos)
 		{
-			size_t pos;
-			while ((pos = _target.find(',')) != std::string::npos)
-			{
-				_targetList.push_back(_target.substr(0,pos));
-				_target.erase(0, pos + 1);
-			}
+			std::cerr << "_target.substr(0,pos) =, size " << _target.substr(0,pos) << " " << _target.substr(0,pos).size() << std::endl;
+			_targetList.push_back(_target.substr(0,pos));
+			_target.erase(0, pos + 1);
 		}
 		_targetList.push_back(_target);
 		std::vector<std::string>::iterator it = _targetList.begin();
 		for (; it != _targetList.end(); ++it)
+			std::cout << "_target list has " << *it << std::endl;
+		it = _targetList.begin();
+		for (; it != _targetList.end(); ++it)
 		{
+			std::cerr << "check if chan exists " << *it << std::endl;
 			if (!_server->doesChannelExist(*it)) {
+				std::cerr << "bye" << std::endl;
 				msg._client->reply(ERR_NOSUCHCHANNEL(_server->getHostname(), msg._client->getNickname(), *it));
 				_targetList.erase(it);
 				msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), *it));
-
 			}
 		}
 		if (_targetList.empty())
@@ -48,12 +52,14 @@ bool	Names::validate(const Message& msg) {
 void	Names::execute(const Message& msg) {
 
 	if (validate(msg)) {
+		std::cerr << "is here " << "with _hasTarget " << _hasTarget << std::endl;
 		if (_hasTarget)
 		{
 			std::vector<std::string>::iterator it = _targetList.begin();
 			std::vector<std::string>::iterator ite = _targetList.end();
 			for (; it != ite; ++it)
 			{
+				std::cerr << "target is " << *it << std::endl;
 				msg._client->reply(RPL_NAMREPLY(_server->getHostname(), msg._client->getNickname(), *it, _server->getChannelPtr(*it)->getMemberList()));
 				msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), *it));
 			}
