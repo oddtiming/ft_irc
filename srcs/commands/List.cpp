9845e01f74f44e9d52cecabea560860464d06b92
Alexandre Dubeau <adubeau@c2r4p1.42quebec.com>
2023-01-26 14:56:45 -0500

    rehauled list & names cmd

--- srcs/commands/List.cpp
+++ srcs/commands/List.cpp
@@ -8,28 +8,39 @@ List::List(Server* server) : Command("list", server) {
 List::~List() {
 
 }
-//#define RPL_LIST(hostname,nick,channel,nbUsers,topic) ":" + (hostname) + " 323 " + (nick) + " " + (channel) + " " + (nbUsers) + " : " + (topic) +  "\r\n" //322
-//#define RPL_LISTEND(host, nick) ":" + (host) + " 323 " + (nick) + " :End of channel list.\r\n" //323
 
 bool	List::validate(const Message& msg) {
-	if (msg.getCommand() == msg.getCommand())
-		return true;
+	if (msg.hasMiddle())
+	{
+		if (msg.hasMiddle() > 0 && msg.getMiddle().at(0).size() > 0) {
+			if (_server->doesChannelExist(msg.getMiddle().at(0))) {
+				_target = msg.getMiddle().at(0);
+				_hasTarget = true;
+			}
+			else
+				return false;
+		}
+	}
 
 	return true;
 }
 
 void	List::execute(const Message& msg) {
 
-	if (validate(msg))
-	{
-		std::map<std::string, Channel *> channelList = _server->getChannelList();
-		//fixme: figure out a way to access the list of channel, most likely a getChannelList in channel.hpp
-		std::map<std::string, Channel *>::iterator it = channelList.begin();
-		for (; it != channelList.end(); ++it)
-		{
-			if (!it->second->checkModes(SECRET) || it->second->isMember(msg._client))
-				msg._client->reply(RPL_LIST(it->first, std::to_string(it->second->getMemberVector().size()), it->second->getTopic()));
+	if (validate(msg)) {
+		if (!_hasTarget) {
+			std::map<std::string, Channel *> channelList = _server->getChannelList();
+			std::map<std::string, Channel *>::iterator it = channelList.begin();
+			for (; it != channelList.end(); ++it) {
+				if (!it->second->checkModes(SECRET) || it->second->isMember(msg._client))
+					msg._client->reply(RPL_LIST(it->first, std::to_string(it->second->getMemberVector().size()),
+												it->second->getTopic()));
+			}
 		}
+		else
+			msg._client->reply(RPL_LIST(_server->getChannelPtr(_target)->getName(),
+										std::to_string(_server->getChannelPtr(_target)->getMemberVector().size()),
+										_server->getChannelPtr(_target)->getTopic()));
 		msg._client->reply(RPL_LISTEND(_server->getHostname(), msg._client->getNickname()));
 
 	}
--- srcs/commands/Names.cpp
+++ srcs/commands/Names.cpp
@@ -8,62 +8,31 @@ Names::Names(Server* server) : Command("names", server) {
 Names::~Names() {
 
 }
-/*
- * issues: /names #new,#new1 duplicates first channel
- * 			/names #new,#new2, segfaults
- * 			/names #new, #new1 segfaults
- * */
+
 bool	Names::validate(const Message& msg) {
-	if (!msg.hasMiddle() || msg.getMiddle().at(0).size() < 1) {
-		_hasTarget = false;
-	}
-	else {
-		_hasTarget = true;
-		_target = msg.getMiddle().at(0);
-		size_t pos;
-		while ((pos = _target.find(',')) != std::string::npos)
-		{
-			std::cerr << "_target.substr(0,pos) =, size " << _target.substr(0,pos) << " " << _target.substr(0,pos).size() << std::endl;
-			_targetList.push_back(_target.substr(0,pos));
-			_target.erase(0, pos + 1);
-		}
-		_targetList.push_back(_target);
-		std::vector<std::string>::iterator it = _targetList.begin();
-		for (; it != _targetList.end(); ++it)
-			std::cout << "_target list has " << *it << std::endl;
-		it = _targetList.begin();
-		for (; it != _targetList.end(); ++it)
-		{
-			std::cerr << "check if chan exists " << *it << std::endl;
-			if (!_server->doesChannelExist(*it)) {
-				std::cerr << "bye" << std::endl;
-				msg._client->reply(ERR_NOSUCHCHANNEL(_server->getHostname(), msg._client->getNickname(), *it));
-				_targetList.erase(it);
-				msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), *it));
+	if (msg.hasMiddle())
+	{
+		if (msg.hasMiddle() > 0 && msg.getMiddle().at(0).size() > 0) {
+			if (_server->doesChannelExist(msg.getMiddle().at(0))) {
+				_target = msg.getMiddle().at(0);
+				_hasTarget = true;
 			}
+			else
+				return false;
 		}
-		if (_targetList.empty())
-			return false;
 	}
 
 	return true;
+
 }
 
 void	Names::execute(const Message& msg) {
 
 	if (validate(msg)) {
-		std::cerr << "is here " << "with _hasTarget " << _hasTarget << std::endl;
 		if (_hasTarget)
 		{
-			std::vector<std::string>::iterator it = _targetList.begin();
-			std::vector<std::string>::iterator ite = _targetList.end();
-			for (; it != ite; ++it)
-			{
-				std::cerr << "target is " << *it << std::endl;
-				msg._client->reply(RPL_NAMREPLY(_server->getHostname(), msg._client->getNickname(), *it, _server->getChannelPtr(*it)->getMemberList()));
-				msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), *it));
-			}
-
+				msg._client->reply(RPL_NAMREPLY(_server->getHostname(), msg._client->getNickname(), _target, _server->getChannelPtr(_target)->getMemberList()));
+				msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), _target));
 		}
 		else
 		{
@@ -72,8 +41,11 @@ void	Names::execute(const Message& msg) {
 			ChannelList::iterator iteC = channelList.end();
 			for (; itC != iteC; ++itC)
 			{
-				msg._client->reply(RPL_NAMREPLY(_server->getHostname(), msg._client->getNickname(), itC->first, itC->second->getMemberList()));
-				msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), itC->first));
+				if (!itC->second->checkModes(SECRET) || itC->second->isMember(msg._client)) {
+					msg._client->reply(RPL_NAMREPLY(_server->getHostname(), msg._client->getNickname(), itC->first,
+													itC->second->getMemberList()));
+					msg._client->reply(RPL_ENDOFNAMES(_server->getHostname(), msg._client->getNickname(), itC->first));
+				}
 			}
 		}
 	}
