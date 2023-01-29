80dd30d0bb9348c0a11bd10fcf766208d2de8885
jgoad <jon@weareopensunday.com>
2023-01-29 17:33:56 -0500

    Finished review of defines.h
    - Removed global OP mode as it was unused
    - Removed DEBUG flag is no longer used
    - Cleaned up defines.h

--- includes/defines.h
+++ includes/defines.h
@@ -3,59 +3,41 @@
 
 #pragma once
 
-// FIXME: set back to 0 before corrections, makes use of the recvmsg() function
-#define DEBUG           1
-
-#define MAX_BUFFER_SIZE 512 /* Maximum read size from recv() in bytes */
-#define MAX_CHANNELS    100 /* Maximum number of channels that can exist on server */
-#define PING_INTERVAL                                                                    \
-	60 /* Interval after which to send a ping to client since their last                 \
-	      activity */
+/* General server settings */
+#define MAX_BUFFER_SIZE 512		/* Maximum read size from recv() in bytes */
+#define MAX_CHANNELS    100		/* Maximum number of channels that can exist on server */
+#define PING_INTERVAL   60		/* Interval after which to send a ping to client since their last activity */
 #define MAX_CONNECTIONS 10
 
 /* Global Modes */
 typedef enum e_globalModes {
-	OP    = 0x1, /* o: global_op (user is a server operator)
-	              * DEPRECATED this fiels is currently unused */
-	AWAY  = 0x2, /* a: away (user mode is set as away)
-	              * INTERNAL USE ONLY, set through /away command. */
-	INVIS = 0x4  /* i: invisible (user should not be announced to channel)
-	              * DEPRECATED this fiels is currently unused, reference client (irssi)
-	              * does not work with it */
+	AWAY  = 0x1, 				/* a: away (user mode is set as away) */
+	INVIS = 0x2 				/* i: invisible (user cannot be seen unless they are known) */
 } t_globalModes;
 
 /* Channel Modes */
 typedef enum e_channelModes {
-	SECRET       = 0x1,  /* s: secret channel (extends private channel functionality,
-	                        cannot be seen by queries for TOPIC, LIST, NAMES) */
-	TOPIC_SET_OP = 0x2,  /* t: topic limit (only +qao can set topic) SET DEFAULT */
-	INV_ONLY     = 0x4,  /* i: invite only (users may only join channel if they have
-	                        been invited by +qao) */
-	NO_MSG_IN    = 0x8,  /* n: no external messages (external messages cannot be seen
-	                        in channel) SET BY DEFAULT */
-	PASS_REQ     = 0x10, /* k: password protected (channel requires a password to join) */
+	SECRET       = 0x1,  		/* s: secret channel (extends private channel functionality, cannot be seen by queries for TOPIC, LIST, NAMES) */
+	TOPIC_SET_OP = 0x2,  		/* t: topic limit (only +qao can set topic) SET DEFAULT */
+	INV_ONLY     = 0x4,  		/* i: invite only (users may only join channel if they have been invited by +qao) */
+	NO_MSG_IN    = 0x8,  		/* n: no external messages (external messages cannot be seen in channel) */
+	PASS_REQ     = 0x10, 		/* k: password protected (channel requires a password to join) */
 } t_channelModes;
 
 /* Channel Member Modes */
 typedef enum e_memberModes {
-	BAN   = 0x1, /* b: ban (prevent user from joining channel) */
-	INVIT = 0x2, /* invited (user has been invite to join this channel)
-	              * INTERNAL USE ONLY, set through /invite command. */
-	C_OP  = 0x4, /* o: operator (3rd highest perm level) */
-	OWNER = 0x8  /* q: owner (highest perm level, only one can exist) */
+	BAN   = 0x1, 				/* b: ban (prevent user from joining channel) */
+	INVIT = 0x2, 				/* invited (user has been invite to join this channel) */
+	C_OP  = 0x4, 				/* o: operator */
+	OWNER = 0x8  				/* q: owner */
 } t_memberModes;
 
+/* Other enums */
 typedef enum e_targetType { CHANNEL = 0x1, USER = 0x2 } t_targetType;
-
-/* Server Status Enum*/
 typedef enum s_serverStatus { OFFLINE = 0, ONLINE = 1, CLOSED = 2 } t_serverStatus;
 
-// NOTE: All +qao members automatically inherit (VOICE | BAN_EXEMPT |
-// INV_EXEMPT) They cannot be placed on ban list, never need an invite, and can
-// always speak
 
-/* Colour defines for terminal */
-
+/* Colour defines for terminal output */
 #define RED     "\033[1;31m"
 #define GREEN   "\033[1;32m"
 #define YELLOW  "\033[1;33m"
@@ -65,64 +47,3 @@ typedef enum s_serverStatus { OFFLINE = 0, ONLINE = 1, CLOSED = 2 } t_serverStat
 #define CLEAR   "\033[0m"
 
 #endif
-
-/* Modes List
-
-/USER
-
-#define REGISTERED		0b00000001
-#define INVISIBLE		0b00000010
-#define RECEIVENOTICE	0b00000100
-#define RECEIVEWALLOPS	0b00001000
-#define IRCOPERATOR		0b00010000
-#define AWAY			0b00100000
-#define PINGING			0b01000000
-#define BREAKCONNECTION	0b10000000
-
-
-1 2 4 8 10 20 40 80 100 200 400
-____________________________________________________
-Global:
-A = Server/Net Admin only channel (settable by Admins)
-
-____________________________________________________
-Channel Member Modes:
-[h] requires at least halfop, [o] requires at least chanop, [q] requires owner
-
-a = Gives Channel Admin to the user
-q = Gives Owner status to the user
-b = Bans the nick!ident@host from the channel.
-       Extended bantypes are also available. (for more info see extended bans
-tutorial) [h] e = Overrides a ban for matching users [h] i = A user must be
-invited to join the channel [h] I = Overrides +i for matching users [h] h =
-Gives HalfOp status to the user (Limited op access)
-
-Maybe:
-k = Users must specify to join (for more info see Using Channel Keys) [h]
-v = Gives Voice to the user (May talk if chan is +m)
-______________________________________________________
-Channel Modes:
-l = Channel may hold at most of users [o]
-
-Maybe:
-t = Only +hoaq may change the topic [h]
-T = No NOTICEs allowed in the channel [o]
-u = Auditorium mode (/names and /who #channel only show channel ops) [q]
-U = Block messages containing mIRC formatting codes [o]
-S = Strips mIRC color codes [o]
-r = The channel is registered (settable by services only)
-R = Only registered (+r) users may join the channel [o]
-p = Private channel (for more info see Using Channel Modes to Hide Your Channel)
-[o] O = IRC Operator only channel (settable by IRCops) L = Channel link (If +l
-is full, the next user will auto-join ) [q] m = Moderated channel (only +vhoaq
-users may speak) [h] M = Must be using a registered nick (+r), or have voice
-access to talk [o] n = Users outside the channel can not send PRIVMSGs to the
-channel [h] K = /KNOCK is not allowed [o] N = No Nickname changes are permitted
-in the channel [o] o = Gives Operator status to the user Q = No kicks allowed
-[o] s = Secret channel (for more info see Using Channel Modes to Hide Your
-Channel) [o] V = /INVITE is not allowed [o]
-
-
-
-
-*/
