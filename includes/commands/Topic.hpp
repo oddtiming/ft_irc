ba973053c3921e37a9c5d263f960c5d6b897202b
jgoad <jon@weareopensunday.com>
2023-01-29 17:24:46 -0500

    Topic review complete
    - Fixed some logic errors and outdated code
    - Will properly send NOTOPIC reply now

--- includes/commands/Topic.hpp
+++ includes/commands/Topic.hpp
@@ -11,19 +11,21 @@
 
 class Topic : public Command
 {
-public:
-	/* Constructors & Destructor */
-	Topic(Server *server);
-	~Topic();
+	public:
+		/* Constructors & Destructor */
+		Topic(Server *server);
+		~Topic() { }
 
-	/* Public Member Functions */
-	bool                validate(const Message& msg);
-	void                execute(const Message& msg);
+		/* Public Member Functions */
+		bool                validate(const Message& msg);
+		void                execute(const Message& msg);
+		void				clearData();
 
-private:
-	Client*				_client;
-	std::string 		_target;
-
+	private:
+		Client*				_client;
+		Channel*			_channel;
+		std::string 		_target;
+		std::string			_topic;
 };
 
 #endif
--- srcs/commands/Topic.cpp
+++ srcs/commands/Topic.cpp
@@ -1,55 +1,77 @@
 #include "../../includes/commands/Topic.hpp"
 
-Topic::Topic(Server* server) : Command("topic", server) {
+Topic::Topic(Server* server) : Command("topic", server) { }
 
-}
-
-Topic::~Topic() {
-
-}
-
+/* Attempt to validate if command can be executed */
 bool	Topic::validate(const Message& msg) {
-	if (msg.getMiddle().empty()) {
-		msg._client->reply(ERR_NEEDMOREPARAMS(_server->getHostname(), _client->getNickname(), msg.getCommand()));
+
+	/* Check if any parameters were passed */
+	if (!msg.hasMiddle())
+	{
+		_client->reply(ERR_NEEDMOREPARAMS(_server->getHostname(), _client->getNickname(), msg.getCommand()));
 		return false;
 	}
 	_target = msg.getMiddle().at(0);
-	if (!_server->doesChannelExist(_target)) {
-		msg._client->reply(ERR_NOSUCHCHANNEL(_server->getHostname(), msg._client->getNickname(), _target));
+	
+	/* Check if target channel exists */
+	if (!_server->doesChannelExist(_target))
+	{
+		_client->reply(ERR_NOSUCHCHANNEL(_server->getHostname(), msg._client->getNickname(), _target));
 		return false;
 	}
-	if (_target.empty())
-		return false;
-    
-	if (_server->getChannelPtr(_target)->checkModes(TOPIC_SET_OP) && !_server->getChannelPtr(_target)->checkMemberModes(msg._client, C_OP)) {
-		msg._client->reply(ERR_CHANOPRIVSNEEDED(_server->getHostname(), _client->getNickname(), _target, 't'));
+	_channel = _server->getChannelPtr(_target);
+
+	/* Check if client is a member of the target channel */
+	if (!_channel->isMember(_client)) {
+		_client->reply(ERR_NOTONCHANNEL(_server->getHostname(), _client->getNickname(), _target));
 		return false;
 	}
-	if (!_server->getChannelPtr(_target)->isMember(msg._client)) {
-		msg._client->reply(ERR_NOTONCHANNEL(_server->getHostname(), _client->getNickname(), _target));
+
+	/* If +t flag is set, check is client has OP privs on target channel */
+	if (_channel->checkModes(TOPIC_SET_OP) && !_channel->checkMemberModes(_client, C_OP)) {
+		_client->reply(ERR_CHANOPRIVSNEEDED(_server->getHostname(), _client->getNickname(), _target, 't'));
 		return false;
 	}
 	return true;
 }
-//fixme: add a reply all to topic
+
+/* Remove data from previous function calls */
+void	Topic::clearData() {
+	_client = nullptr;
+	_channel = nullptr;
+	_target.clear();
+	_topic.clear();
+}
+
+/* Attempt to validate and execute command */
 void	Topic::execute(const Message& msg) {
+	/* Clear data from previous function calls */
+	clearData();
 	_client = msg._client;
+
+
 	if (validate(msg))
 	{
-		if (msg.getTrailing().size() == 1 && msg.getTrailing().at(0) == ':') {
-			_server->getChannelPtr(_target)->setTopic("");
-			_server->getChannelPtr(_target)->sendToAll(":" + _buildPrefix(msg) + " TOPIC " + _target + " : " + "\r\n");
-		}
-		else if (msg.hasTrailing()) {
-			_server->getChannelPtr(_target)->setTopic(msg.getTrailing());
-			_server->getChannelPtr(_target)->sendToAll(":" + _buildPrefix(msg) + " TOPIC " + _target + " :"+ msg.getTrailing() + "\r\n");
-	}
-		else if (!msg.hasTrailing() && msg.getMiddle().size() > 1)
+		/* If message has trailing but trailing is empty, clear topic */
+		if (msg.hasTrailing() && msg.getTrailing().empty())
 		{
-			_server->getChannelPtr(_target)->setTopic(msg.getMiddle().at(1));
-			_server->getChannelPtr(_target)->sendToAll(":" + _buildPrefix(msg) + " TOPIC " + _target + " :"+ msg.getMiddle().at(1) + "\r\n");
+			_channel->setTopic("");
+			_channel->sendToAll(":" + _buildPrefix(msg) + " TOPIC " + _target + " :" + "\r\n");
 		}
+		/* If message has trailing and trailing is not empty, set the topic */
+		else if (msg.hasTrailing() && !msg.getTrailing().empty())
+		{
+			_channel->setTopic(msg.getTrailing());
+			_channel->sendToAll(":" + _buildPrefix(msg) + " TOPIC " + _target + " :"+ msg.getTrailing() + "\r\n");
+		}
+		/* If message does not have trailing, send the current topic */
 		else
-			msg._client->reply(RPL_TOPIC(_server->getHostname(), _client->getNickname(), _target, _server->getChannelPtr(_target)->getTopic()));
+		{
+			_topic = _channel->getTopic();
+			if (_topic.empty())
+				_client->reply(RPL_NOTOPIC(_server->getHostname(), _client->getNickname(), _target));
+			else
+				_client->reply(RPL_TOPIC(_server->getHostname(), _client->getNickname(), _target, _topic));
+		}
 	}
 }
