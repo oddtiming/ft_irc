44d3a40969bdda98152f1f569bcae4f672e2f74d
oddtiming <36460419+oddtiming@users.noreply.github.com>
2023-01-28 21:46:29 -0500

    Reverted the changed to shared files

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -1,5 +1,5 @@
 #ifndef COMMAND_HPP
-#define COMMAND_HPP
+# define COMMAND_HPP
 
 #pragma once
 
@@ -9,39 +9,40 @@
 /* Local Includes */
 #include "Message.hpp"
 #include "Server.hpp"
-#include "defines.h"
 #include "replies.h"
+#include "defines.h"
 
 /* Class Prototypes */
 
-class Command
-{
-  public:
-	/* Constructors & Destructor */
 
-	virtual ~Command( ) {}
+class Command {
+	public:
+		/* Constructors & Destructor */
+		
+		virtual ~Command() { }
 
-	/* Operator Overloads */
+		/* Operator Overloads */
 
-	/* Setters & Getters */
+		/* Setters & Getters */
 
-	/* Public Member Functions */
-	virtual void execute(const Message& msg) = 0;
+		/* Public Member Functions */
+		virtual void	execute(const Message& msg) = 0;
 
-  protected:
-	Command(const std::string& name, Server* server) : _name(name), _server(server) {}
-	const std::string _name;
-	bool              _channelOpRequired;
-	bool              _globalOpRequired;
-	int               _replCode;
-	Server*           _server;
+	protected:
+		Command(const std::string& name, Server * server) : _name(name), _server(server) { }
+		const std::string	_name;
+		bool				_channelOpRequired;
+		bool				_globalOpRequired;
+		int					_replCode;
+		Server* 			_server;
 
-	/* Member functions */
-	std::string _buildPrefix(const Message& msg) {
-		return msg._client->getNickname( ) + "!" + msg._client->getUsername( ) + "@"
-		       + msg._client->getHostname( );
-		// FIXME: DO NOT FORGET TO CHANGE THIS, hardcoded for testing
-	}
+		/* Member functions */
+		std::string			_buildPrefix(const Message& msg) { 
+			return msg._client->getNickname() + "!"
+				 + msg._client->getUsername() + "@"
+				 + msg._client->getHostname();
+				 //FIXME: DO NOT FORGET TO CHANGE THIS, hardcoded for testing
+		}
 };
 
 #endif
